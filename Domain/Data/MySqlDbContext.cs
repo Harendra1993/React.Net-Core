aace2845c81d54dd7ccebf04cd3f4c02147c9270
patrix <patrix@example.com>
2020-04-29 09:40:26 +0200

    Initial Commit

---
+++ Domain/Data/MySqlDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using PPR.Domain.Models;
+
+namespace PPR.Domain.Data {
+    public class MySqlDbContext {
+
+    }
+}
---
+++ Domain/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PPR.Domain.Models {
+    public class ApplicationUser : IdentityUser { }
+}
