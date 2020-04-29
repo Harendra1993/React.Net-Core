f4b3143ec1732752e838b8076fe6a721fff6bb69
patrix <patrix@example.com>
2020-04-29 12:54:17 +0200

    Added Jwt Auth in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -5,6 +7,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace PPR {
     public class Startup {
@@ -16,6 +19,18 @@ namespace PPR {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer (options => {
+                    options.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration.GetValue<string> ("JwtIssuer"),
+                    ValidAudience = Configuration.GetValue<string> ("JwtAudience"),
+                    IssuerSigningKey = new SymmetricSecurityKey (Encoding.UTF8.GetBytes (Configuration.GetValue<string> ("JwtSecretKey")))
+                    };
+                });
 
             services.AddControllersWithViews ();
 
@@ -38,6 +53,8 @@ namespace PPR {
             app.UseHttpsRedirection ();
             app.UseStaticFiles ();
             app.UseSpaStaticFiles ();
+
+            app.UseAuthentication ();
 
             app.UseRouting ();
 
