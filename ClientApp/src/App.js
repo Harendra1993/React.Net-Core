2621c74e445f2812c0aa1ffa0ecc74c84af580d4
patrix <patrix@example.com>
2020-05-05 10:18:53 +0200

    Added Navbar and Dark mode implemented

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,141 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import Navbar from './components/navbar';
+import ScrollToTop from './utils/ScrollToTop';
 
-import './App.scss'
+import React, { Suspense, lazy } from 'react';
+// import {Helmet} from 'react-helmet';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import { useLocalStorage, useEffectOnce } from 'react-use';
 
-export default class App extends Component {
-  static displayName = App.name;
+import './App.scss';
 
-  render() {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+// const DeepDive = lazy(() => import('./components/deepdive'));
+// const FAQ = lazy(() => import('./components/faq'));
+// const PatientDB = lazy(() => import('./components/patientdb'));
+// const Resources = lazy(() => import('./components/resources'));
+// const State = lazy(() => import('./components/state'));
+
+
+
+function App() {
+  const pages = [
+    {
+      pageLink: '/',
+      view: Home,
+      displayName: 'Home',
+      animationDelayForNavbar: 0.2,
+      showInNavbar: true,
+    },
+    // {
+    //   pageLink: '/demographics',
+    //   view: PatientDB,
+    //   displayName: 'Demographics',
+    //   animationDelayForNavbar: 0.3,
+    //   showInNavbar: true,
+    // },
+    // {
+    //   pageLink: '/deepdive',
+    //   view: DeepDive,
+    //   displayName: 'Deep Dive',
+    //   animationDelayForNavbar: 0.4,
+    //   showInNavbar: true,
+    // },
+    // {
+    //   pageLink: '/essentials',
+    //   view: Resources,
+    //   displayName: 'Essentials',
+    //   animationDelayForNavbar: 0.5,
+    //   showInNavbar: true,
+    // },
+    // {
+    //   pageLink: '/faq',
+    //   view: FAQ,
+    //   displayName: 'FAQ',
+    //   animationDelayForNavbar: 0.6,
+    //   showInNavbar: true,
+    // },
+    // {
+    //   pageLink: '/state/:stateCode',
+    //   view: State,
+    //   displayName: 'State',
+    //   animationDelayForNavbar: 0.7,
+    //   showInNavbar: false,
+    // },
+  ];
+
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', false);
+  const [isThemeSet] = useLocalStorage('isThemeSet', false);
+
+  useEffectOnce(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches &&
+      !isThemeSet
+    ) {
+      setDarkMode(true);
+    } else if (
+      window.matchMedia &&
+      !window.matchMedia('(prefers-color-scheme: dark)').matches &&
+      !isThemeSet
+    ) {
+      setDarkMode(false);
+    }
+  });
+
+  React.useEffect(() => {
+    if (darkMode) {
+      document.querySelector('body').classList.add('dark-mode');
+    } else {
+      document.querySelector('body').classList.remove('dark-mode');
+    }
+  }, [darkMode]);
+
+  return (
+    <div className="App">
+      {/* <Helmet>
+        <script type="application/ld+json">
+          {JSON.stringify(schemaMarkup)}
+        </script>
+      </Helmet> */}
+
+      <Router>
+        <ScrollToTop />
+        <Suspense fallback={<div className="lazy"></div>}>
+          <Route
+            render={({ location }) => (
+              <div className="Almighty-Router">
+                <Navbar
+                  pages={pages}
+                  darkMode={darkMode}
+                  setDarkMode={setDarkMode}
+                />
+                <Switch location={location}>
+                  {pages.map((page, index) => {
+                    return (
+                      <Route
+                        exact
+                        path={page.pageLink}
+                        render={({ match }) => (
+                          <page.view key={match.params.stateCode || index} />
+                        )}
+                        key={index}
+                      />
+                    );
+                  })}
+                  <Redirect to="/" />
+                </Switch>
+              </div>
+            )}
+          />
+        </Suspense>
+      </Router>
+    </div>
+  );
 }
+
+export default App;
--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export class Home extends Component {
   static displayName = Home.name;
 
-  render () {
+  render() {
     return (
-      <div>
+      <React.Fragment>
         <h1>Hello, world!</h1>
         <p>Welcome to your new single-page application, built with:</p>
         <ul>
@@ -20,7 +20,7 @@ export class Home extends Component {
           <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
         </ul>
         <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
+      </React.Fragment>
     );
   }
 }
