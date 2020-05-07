f779351a5b31d8fcdfc611047d1f32232d2ef532
patrix <patrix@example.com>
2020-05-07 09:49:34 +0200

    Folder structure refactoring

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import './css/style.css';
+import './assets/css/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
